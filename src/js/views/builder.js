db6ae60fa55e4432d21af37488d714c4465110f1
Matt Hampel <matth@localdata.com>
2013-11-21 13:13:02 -0500

    Form editor upgrades

--- src/js/views/builder.js
+++ src/js/views/builder.js
@@ -35,19 +35,21 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
       answer: _.template($('#answer-edit-template').html())
     },
 
+    events: {
+      'click .save': 'save'
+    },
+
     initialize: function(options) {
       _.bindAll(this,
         'render',
         'save',
-        'done',
 
         'makeBlankQuestion',
         'renderForm',
         'suffix',
         'slugify',
-        'updatePreview',
 
-        'editQuestion',
+        'editQuestionFactory',
         'setQuestionType',
         'deleteQuestion',
         'createQuestion',
@@ -65,7 +67,6 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
 
     render: function() {
       this.renderForm();
-      this.updatePreview();
     },
 
     save: function(event) {
@@ -77,13 +78,6 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
         console.log('Form successfully saved');
         $(".saved").fadeIn().css("display","inline-block").delay(2000).fadeOut();
       });
-    },
-
-    done: function(event) {
-      event.preventDefault();
-      console.log('Done editing');
-      _kmq.push(['record', 'Done editing survey questions']);
-      this.trigger("done");
     },
 
     // The blank question template.
@@ -112,10 +106,6 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
       this.formQuestions = $('#editor');
       this.formQuestions.html('');
 
-      // Handle save events
-      $(this.el).find('.save').click(this.save);
-      $(this.el).find('.done').click(this.done);
-
       // Default to a blank question if the form is empty
       if (settings.formData === undefined) {
         settings.formData = {};
@@ -126,10 +116,7 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
       // Render form
       _.each(settings.formData.questions, function (question, questionIndex) {
         this.renderQuestion(question, undefined, undefined, undefined, undefined, questionIndex, settings.formData.questions);
-
-        // last param was settings.formData.questions
       }, this);
-      this.updatePreview();
     },
 
     suffix: function(name) {
@@ -142,13 +129,7 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
       return '';
     },
 
-    editQuestion: function(question) {
-      // TODO:
-      // What if we do:
-      // view = this;
-      // return function(event) {
-      //  view.slugify(text)
-      //  ...
+    editQuestionFactory: function(question) {
       return function(event) {
         console.log('Updating question');
         _kmq.push(['record', 'Question edited']);
@@ -164,8 +145,6 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
             answer.value = 'yes';
           }.bind(this));
         }
-
-        this.updatePreview();
       };
     },
 
@@ -204,12 +183,11 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
         console.log("removing question");
         _kmq.push(['record', 'Question deleted']);
 
-        // Remove it from the DON.
+        // Remove it from the DOM.
         $question.remove();
 
         // Remove it from the json
         parent.splice(questionIndex, 1);
-        this.updatePreview();
       };
     },
 
@@ -225,7 +203,6 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
         this.setQuestionType(newQuestion, type);
         parent.splice(questionIndex + 1, 0, newQuestion);
 
-        this.updatePreview();
         this.renderForm();
       };
     },
@@ -240,7 +217,6 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
         delete newQuestion.answers;
         parent.splice(questionIndex + 1, 0, newQuestion);
 
-        this.updatePreview();
         this.renderForm();
       };
     },
@@ -254,12 +230,11 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
         });
         this.setQuestionLayout(question);
 
-        this.updatePreview();
         this.renderForm();
       };
     },
 
-    addSubQuestion: function(question, index) {
+    addSubQuestionFactory: function(question, index) {
       return function(event) {
         event.preventDefault();
         var type = $(event.target).attr('data-type');
@@ -277,7 +252,6 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
           question.answers[index].questions = [newQuestion];
         }
 
-        this.updatePreview();
         this.renderForm();
       };
     },
@@ -295,7 +269,6 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
           question.answers[index].value = 'yes';
         }
 
-        this.updatePreview();
       };
     },
 
@@ -306,7 +279,6 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
 
         // Remove it from the json
         question.answers.splice(index, 1);
-        this.updatePreview();
 
         this.setQuestionLayout(question);
 
@@ -330,6 +302,14 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
       // TODO:
       // This should pass around a well-document options object
       // Instead of 30 different parameters
+      // var options = {
+      //   visible:
+      //   parentId:
+      //   triggerId:
+      //   appendTo:
+      //   questionIndex:
+      //   parent:
+      // }
 
       // Set default values for questions
       if (visible === undefined) {
@@ -358,7 +338,7 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
       // TODO: make it less verbose
 
       // Listen for changes to the question text
-      var editQuestionProxy = $.proxy(this.editQuestion(question), this);
+      var editQuestionProxy = $.proxy(this.editQuestionFactory(question), this);
       $question.find('> div input').keyup(editQuestionProxy);
 
       // Listen for a request to remove a question
@@ -408,12 +388,12 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
 
         if(question.type === "checkbox") {
           suffixed_name = answer.name + this.suffix(answer.name);
-          triggerID = suffixed_name; //_.uniqueId(answer.name);
-          id = suffixed_name; //_.uniqueId(answer.name);
+          triggerID = suffixed_name;
+          id = suffixed_name;
         }
 
         // Set the data used to render the answer
-        var data = {
+        var answerData = {
           questionName: suffixed_name,
           id: triggerID,
           theme: (answer.theme || "c"),
@@ -425,7 +405,7 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
         var $answer;
         var referencesToAnswersForQuestion;
 
-        $answer = $(this.templates.answer(data));
+        $answer = $(this.templates.answer(answerData));
         referencesToAnswersForQuestion = this.boxAnswersByQuestionId[questionID];
         if (referencesToAnswersForQuestion === undefined) {
           referencesToAnswersForQuestion = [];
@@ -442,9 +422,18 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
         var editAnswerProxy = $.proxy(this.editAnswer(question, index), this);
         $input.keyup(editAnswerProxy);
 
+        // Show add sub-question
+
+        // Hide add sub-question
+        $answer.find('.close-add-sub-question').click(function(event) {
+          event.preventDefault();
+          $(this).parent().hide();
+        });
+
+
         // Add a sub-question
         var $addSubQuestion = $answer.find('.add-sub-question');
-        var addSubQuestionProxy = $.proxy(this.addSubQuestion(question, index), this);
+        var addSubQuestionProxy = $.proxy(this.addSubQuestionFactory(question, index), this);
         $addSubQuestion.click(addSubQuestionProxy);
 
         // Remove an answer
@@ -475,10 +464,6 @@ function($, _, Backbone, _kmq, settings, api, FormViews) {
       text = text.replace(/\s/gi, "-");
       text = text.replace(/,/gi, '');
       return text;
-    },
-
-    updatePreview: function() {
-      this.trigger("formUpdated");
     }
 
   }); // end BuilderView{}
