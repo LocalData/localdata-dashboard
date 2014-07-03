c885315e52e51dc8c28e5276729156b61b153dbd
Matt Hampel <matth@localdata.com>
2014-07-03 11:43:07 -0400

    Remove the d3/epoch junk

--- src/js/main.js
+++ src/js/main.js
@@ -9,8 +9,6 @@ require.config({
     jquery: 'lib/jquery-1.7.1',
     backbone: 'lib/backbone',
     Chart: 'lib/Chart',
-    d3: 'lib/d3.min',
-    epoch: 'lib/epoch.0.5.2.min',
     moment: 'lib/moment.min',
     'lib/kissmetrics': '//doug1izaerwt3.cloudfront.net/' + '1f57015c5e8f46bdc07701e1aa74c6cbdf565383' + '.1',
     'lib/kmq': 'lib/kissmetrics'
@@ -25,8 +23,6 @@ require.config({
       deps: ['lib/lodash', 'jquery'],
       exports: 'Backbone'
     },
-
-    epoch: ['d3.global', 'jquery'],
 
     'lib/leaflet/leaflet': {
       exports: 'L'
@@ -58,17 +54,10 @@ require.config({
   }
 });
 
-// Hack to make d3 available to less-sophisticated third-party plugins
-// https://github.com/mbostock/d3/issues/1693#issuecomment-35556356
-define('d3.global', ['d3'], function(_) {
-  d3 = _;
-});
 
-require(['jquery', 'lib/lodash', 'loglevel', 'backbone', 'app', 'd3', 'epoch'],
+require(['jquery', 'lib/lodash', 'loglevel', 'backbone', 'app'],
         function ($, _, logLevel, Backbone, app, epoch) {
   'use strict';
-
-  console.log("DO we have d3", epoch);
 
   logLevel('verbose');
 
